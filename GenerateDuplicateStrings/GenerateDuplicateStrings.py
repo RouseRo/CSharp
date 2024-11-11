4242eaee9ab1622d7cad64703eb8d60df2d01fd9
Robert K Rouse <robert_k_rouse99@yahoo.com>
2024-11-11 12:52:23 -0800

    Pushing all staged changes

---
+++ GenerateDuplicateStrings/GenerateDuplicateStrings.py
@@ -0,0 +1,27 @@
+#==================================================================================================
+#==================================================================================================
+#
+# Title: Generate Duplicate Strings Project
+# Author: Robert Rouse
+# Created Date: 10-18-2020, 20:38
+# last Updated: 
+#
+# Purpose:
+#   This program generates complex data sets of duplicate strings for testing the
+#   methods in the Find Duplicate Strings Project.
+#
+# Features:
+#
+# History:
+#
+#==================================================================================================
+#==================================================================================================
+
+import asyncio
+
+async def main():
+    print("Generate Duplicates Strings Main Start")
+    print()
+
+asyncio.run(main())
+
